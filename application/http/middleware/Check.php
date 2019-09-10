411d0e5fb9004977ba927ab79251b6744a88d336
CKJ <994539036@QQ.COM>
2019-09-10 17:09:35 +0800

    first pull

---
+++ application/http/middleware/Check.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\http\middleware;
+
+class Check
+{
+    public function handle($request, \Closure $next)
+    {
+        if (preg_match('~micromessenger~i', $request->header('user-agent'))) {
+            $request->InApp = 'WeChat';
+        } else if (preg_match('~alipay~i', $request->header('user-agent'))) {
+            $request->InApp = 'Alipay';
+        }
+        return $next($request);
+    }
+}
